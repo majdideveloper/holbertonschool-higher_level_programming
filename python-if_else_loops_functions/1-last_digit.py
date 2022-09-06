0e9a0ff9eea3d4d17751b17530321e8374694249
majdi aribi <majdi.developer@gmail.com>
2022-09-06 17:10:06 +0100

    :white_check_mark: else if python :white_check_mark:

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ sign = 1
 if number < 0:
     number = -1 * number
     sign = -1
-num = (number % 10) * sign
+num = (number % 10) 
 if num > 5:
     print(f"Last digit of {number * sign} is {num * sign} and is greater than 5")
 elif num == 0:
