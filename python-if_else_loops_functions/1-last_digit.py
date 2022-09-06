041041daef3c7a5e318505875c96b25f71374b2c
majdi aribi <majdi.developer@gmail.com>
2022-09-06 17:02:15 +0100

    :white_check_mark: else if python :white_check_mark:

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-num = number % 10
-if number > 5:
-    print(f"Last digit of {number} is {num} and is greater than 5")
-elif number == 0:
-    print(f"Last digit of {number} is {num} and is 0")
-elif number < 6 and number != 0:
-    print(f"Last digit of {number} is {num} and is less than 6 and not 0")
+sign = 1
+if number < 0:
+    number = -1 * number
+    sign = -1
+num = (number % 10)
+if num > 5:
+    print(f"Last digit of {number * sign} is {num * sign} and is greater than 5")
+elif num == 0:
+    print(f"Last digit of {number * sign} is {num * sign} and is 0")
+elif num < 6 and num != 0:
+    print(f"Last digit of {number * sign} is {num * sign} and is less than 6 and not 0")
 
