c8204f0a61386d3e766cc702ebf0e8a73620615d
majdi aribi <majdi.developer@gmail.com>
2022-09-06 16:23:48 +0100

    :tada: if else in python :tada:

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 num = number % 10
-if number == 0:
+if number > 5:
     print(f"Last digit of {number} is {num} and is greater than 5")
-elif number > 5:
+elif number == 0:
     print(f"Last digit of {number} is {num} and is 0")
 elif number < 6 and number != 0:
     print(f"Last digit of {number} is {num} and is less than 6 and not 0")
