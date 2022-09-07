d79d4c18ebd76252fa1bf98fda496407628717ce
majdi aribi <majdi.developer@gmail.com>
2022-09-07 09:39:42 +0100

    :recycle: if else python :recycle:

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
-for char in range (97,122):
-    print("{}".format(chr(char)),end="")
+for char in range(97, 122):
+    print("{}".format(chr(char)), end="")
--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
-for char in range (97,122):
-    if char != 113 and char !=101:
-        print("{}".format(chr(char)) ,end="")
+for char in range(97, 122):
+    if char != 113 and char != 101:
+        print("{}".format(chr(char)), end="")
