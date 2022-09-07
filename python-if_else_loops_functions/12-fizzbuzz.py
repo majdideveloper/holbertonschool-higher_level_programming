9bb46e89a6aa202414aaaa903d8ed3beee8bfd21
majdi aribi <majdi.developer@gmail.com>
2022-09-07 14:28:16 +0100

    :recycle: if else python :recycle:

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for num in range(1,101):
+    for num in range(1, 101):
         if num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz ", end="")
         elif num % 3 == 0:
@@ -8,4 +8,4 @@ def fizzbuzz():
         elif num % 5 == 0:
             print("Buzz ", end="")
         else:
-           print(f"{num} ", end="")
+            print(f"{num} ", end="")
