3acbe1080c1db935e5558eb8d3bf63433c886af4
majdi aribi <majdi.developer@gmail.com>
2022-09-08 18:13:14 +0100

    :bulb: import modules:bulb:

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,11 +3,12 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
-    res = 0
     lengArg = len(sys.argv)
     if lengArg != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
 
     validOp = "+-*/"
     valid = validOp.find(sys.argv[2])
@@ -15,18 +16,25 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
+
     if valid == 0:
-        res = add(sys.argv[1], sys.argv[3])
-        print(f"""{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {res}""")
+        print("{} + {} = {}".format(a, b, add(a, b)))
+
 
     if valid == 1:
-        res = sub(sys.argv[1], sys.argv[3])
-        print(f"""{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {res}""")
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+
 
     if valid == 2:
-        res = mul(sys.argv[1], sys.argv[3])
-        print(f"""{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {res}""")
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+
 
     if valid == 3:
-        res = div(sys.argv[1], sys.argv[3])
-        print(f"""{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {res}""")
+        print("{} / {} = {}".format(a, b, div(a, b)))
+
+
+
+
+
+
+
