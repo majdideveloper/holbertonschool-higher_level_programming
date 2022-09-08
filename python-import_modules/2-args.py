409c584c23f5e4e18b4136d1303b7e7678cc9a6d
majdi aribi <majdi.developer@gmail.com>
2022-09-08 13:53:29 +0100

    :bulb: import modules:bulb:

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     lengArg = len(sys.argv)
     if lengArg <= 1:
         print("0 arguments.")
+    elif lengArg == 2:
+    print(f"{lengArg - 1} argument:")
     else:
         print(f"{lengArg - 1} arguments:")
     for item in range(1, lengArg):
