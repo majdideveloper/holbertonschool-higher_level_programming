1b53aa0d34f9b9e216fe8cc10088022c0a24a044
majdi aribi <majdi.developer@gmail.com>
2022-09-13 16:09:01 +0100

    :tada: data structures :tada:

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    temp = 0
+    if len(my_list) == 0:
+        return None
+    temp = my_list[0]
     for item in my_list:
         if item > temp:
             temp = item
