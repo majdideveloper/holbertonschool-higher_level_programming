4b74d951adaaf71bab38200aa7e2652d5b20b7b3
majdi aribi <majdi.developer@gmail.com>
2022-09-13 16:01:22 +0100

    :tada: data structures :tada:

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        a = None
+        return len(sentence), None
     a = len(sentence)
     b = sentence[0]
     new_tuple = (a, b)
