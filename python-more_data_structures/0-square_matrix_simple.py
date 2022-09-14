5190dec8abf21c7bd54028e63f66d8cb969105d7
majdi aribi <majdi.developer@gmail.com>
2022-09-14 08:40:12 +0100

    :construction: more data structures :construction:

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    new_matrix = []
     for i in range(len(matrix)):
+        new_matrix.append([])
         for j in range(len(matrix[0])):
-            new_matrix[i][j] = matrix[i][j] * matrix[i][j]
+            new_matrix[i].append(matrix[i][j] * matrix[i][j])
     return new_matrix
