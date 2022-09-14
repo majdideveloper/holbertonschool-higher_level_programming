c89916b470aef702b07ccba090a8103dfa8615fc
majdi aribi <majdi.developer@gmail.com>
2022-09-14 15:38:14 +0100

    :construction: more data structures :construction:

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    best_value = 0
+    list_val = list(a_dictionary.values())
+    list_key = list(a_dictionary.keys())
+    for value in list_val:
+        if value > best_value:
+            best_value = value
+    position = list_val.index(best_value)
+    return list_key[position]
