31de3cfc9514991d9057cc40426be0cdb75875de
majdi aribi <majdi.developer@gmail.com>
2022-09-15 08:58:35 +0100

    :goal_net: exceptions :goal_net:

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(0, int(x)):
-        try:
-            print(f"{my_list[i]}", end="")
-            if i == x - 1:
-                print("")
-        except IndexError:
-            print("")
-            return i
-    return i + 1
+    try:
+        idx = 0
+        for x in range(x):
+            print('{}'.format(my_list[x]), end="")
+            idx += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return idx
