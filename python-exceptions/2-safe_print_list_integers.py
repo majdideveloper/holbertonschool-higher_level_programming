b33fed24592ab29b79607be1a5d9c465bd53d348
majdi aribi <majdi.developer@gmail.com>
2022-09-16 16:36:44 +0100

    :bug: try exceptions:bug:

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,6 @@ def safe_print_list_integers(my_list=[], x=0):
     for idx in range(x):
         if x == idx:
             break
-        if idx >= len(my_list):
-            raise IndexError
         try:
             print("{:d}".format(my_list[idx]), end="")
             num_print += 1
