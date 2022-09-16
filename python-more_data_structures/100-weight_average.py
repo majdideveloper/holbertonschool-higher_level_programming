bff1a7ae489ed3ad3c695256775bc7420268e737
majdi aribi <majdi.developer@gmail.com>
2022-09-16 10:18:36 +0100

    :tada: advanced task

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -4,12 +4,14 @@ def weight_average(my_list=[]):
         return 0
     sum_all_list = 0
     sum_div = 0
+    i = 1
     for row in my_list:
         list_tuple = list(row)
         sum_tuple = 1
         for item in list_tuple:
-            if item == list_tuple[-1]:
+            if item == list_tuple[-1] and i != len(my_list):
                 sum_div += item
             sum_tuple *= item
         sum_all_list += sum_tuple
+        i += 1
     return (sum_all_list / sum_div)
