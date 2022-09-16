b14fce9433c4aec311a7bb476f70f82f4b52d7d4
majdi aribi <majdi.developer@gmail.com>
2022-09-16 09:14:09 +0100

    fix :bug: in task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for item in row:
-            print("{:d}".format(item), end=" ")
-        print("")
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j == len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end="")
+                break
+            print("{}".format(matrix[i][j]), end=" ")
+        print()
