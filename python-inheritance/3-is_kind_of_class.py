1b082ec1f6650bb0ed1aad0661f6b1929a59ccd0
majdi aribi <majdi.developer@gmail.com>
2022-09-27 10:09:30 +0100

    :white_check_mark: inheritance python  :white_check_mark:

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""Defines a class and inherited class-checking function."""
+
+
 def is_kind_of_class(obj, a_class):
+    """Check if obj is instance from the class.
+     Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is  an instance of a_class - True.
+        Otherwise - False.
+    """
     return isinstance(obj, a_class)
