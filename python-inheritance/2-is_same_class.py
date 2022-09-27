50195dc904aea335ebf53850055d5a61979ae4ce
majdi aribi <majdi.developer@gmail.com>
2022-09-27 10:03:48 +0100

    :white_check_mark: inheritance python  :white_check_mark:

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""Defines a class-checking function."""
+
+
 def is_same_class(obj, a_class):
+    """check if an object is the same type of class
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    """
     if type(obj) == a_class:
         return True
     return False
