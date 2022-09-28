1fe485ee6ac337ff5d5e6db1b8b9d1a8191d4c73
majdi aribi <majdi.developer@gmail.com>
2022-09-28 08:33:03 +0100

    :white_check_mark: inheritance python  :white_check_mark:

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Define  a  class BaseGeometry inherited class object."""
+
+
 class BaseGeometry(object):
+    """Represent a BaseGeometry class inherited class object."""
+
     def area(self):
+        """Return Exception area is not implemented."""
         raise Exception("area() is not implemented")
