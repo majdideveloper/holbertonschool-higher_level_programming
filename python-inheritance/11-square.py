66ca5e7ebe7ea2d2953d3c5d7ca44da5d6b1e31f
majdi aribi <majdi.developer@gmail.com>
2022-09-28 09:05:55 +0100

    :white_check_mark: inheritance python  :white_check_mark:

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Define  a  class Square inherited class Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a Rectangle class inherited class BaseGeometry."""
+
+    def __init__(self, size):
+        """Intialize a new instance from Square
+
+        Args:
+            size (int):The size of The new Square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Intialize a new instance from Square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return String [Square] size/size"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
