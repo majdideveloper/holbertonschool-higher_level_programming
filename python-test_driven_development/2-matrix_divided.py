8f4c5916a9440fc694862a4705b442043344b88a
majdi aribi <majdi.developer@gmail.com>
2022-09-28 10:57:16 +0100

    :clown_face: create Test Python :clown_face:

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""function divide matrix """
+
+
+def matrix_divided(matrix, div):
+    """function divide matrix and return matrix of resultat"""
+    for row in matrix:
+        if not all(isinstance(item, (int, float)) for item in row):
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [list(map(lambda item: round(item / div, 2), row))
+            for row in matrix]
