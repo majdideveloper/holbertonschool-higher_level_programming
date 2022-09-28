4ffaf03a4f4ba3d9d2bbe3f232abfb715a696d2e
majdi aribi <majdi.developer@gmail.com>
2022-09-28 14:30:30 +0100

    fix  test to task 1 and 7

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""Defines an class MyList."""
+
+
 class MyList(list):
+    """Defines an class MyList."""
+
     def __init__(self):
+        """function Initialize."""
         list.__init__(self)
 
     def print_sorted(self):
+        """Return sorted list."""
         print(sorted(self))
 
