bb83c05a7b82ef4e227ebcd51f70f353fb32cb68
majdi aribi <majdi.developer@gmail.com>
2022-09-28 08:27:24 +0100

    :white_check_mark: inheritance python  :white_check_mark:

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""Defines a class and inherited class-checking function."""
+
+
 def inherits_from(obj, a_class):
-    if type(obj)==a_class:
+        """Check if obj is an inherited instance from the class.
+     Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is  an inherited instance of a_class - True.
+        Otherwise - False.
+    """
+
+    if type(obj) == a_class:
         return False
     return isinstance(obj, a_class)
