ce3e95d96db81e9d25e312f6ccddd1a559dc1af9
majdi aribi <majdi.developer@gmail.com>
2022-09-28 10:08:25 +0100

    :clown_face: create Test Python :clown_face:

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """function add two integer"""
 
+
 def add_integer(a, b=98):
     """function add_integer return sum two integer"""
-    if type(a) not in [int ,float]:
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) not in [int ,float]:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    if type(a) is float:
+    if isinstance(a, float):
         a = int(a)
-    if type(b) is float:
+    if isinstance(b, float):
         b = int(b)
     return a + b
