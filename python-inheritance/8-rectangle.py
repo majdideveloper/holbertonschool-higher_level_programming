d7d329e2b1834f2d917c735c746577dc36b1c785
majdi aribi <majdi.developer@gmail.com>
2022-09-28 08:39:24 +0100

    :white_check_mark: inheritance python  :white_check_mark:

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
+"""Define  a  class Rectangle inherited class BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
+    """Represent a Rectangle class inherited class BaseGeometry."""
+
     def __init__(self, width, height):
-      self.integer_validator("width", width)
-      self.__width = width
-      self.integer_validator("height", height)
-      self.__height = height
+        """Intialize a new instance from Rectangle
+
+        Args:
+            width (int):The width of The new Rectangle
+            height (int):The height oof The new Rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
