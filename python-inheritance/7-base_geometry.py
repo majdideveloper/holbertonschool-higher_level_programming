dcd9637f24f998888c5d98046dfb2aad7d61eb1c
majdi aribi <majdi.developer@gmail.com>
2022-09-28 08:46:34 +0100

    :white_check_mark: inheritance python  :white_check_mark:

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
+"""Define  a  class BaseGeometry inherits class object"""
+
+
 class BaseGeometry(object):
+    """Represent a BaseGeometry class."""
+
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Check if value is int and greater than 0.
+        Args:
+            name:string name of int.
+            value:int to verfy.
+        Returns:
+            Exception TypeError if type(value) not int
+            or Exception ValueError if value <= 0.
+            """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
