22340c6155319a42de18846a8bb2b22c25e4db4f
majdi aribi <majdi.developer@gmail.com>
2022-09-28 17:20:11 +0100

    write a function return String

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function say_my_name """
+
+
+def say_my_name(first_name, last_name=""):
+    """Function say_my_name and return string."""
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
