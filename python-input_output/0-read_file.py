5bf81f9607e2ca6ce9fc0f1755dfc27672cb6675
majdi aribi <majdi.developer@gmail.com>
2022-10-03 08:30:43 +0100

    :memo: read file and print as output :memo:

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""this module contains function that read file and print as output"""
+
 
 def read_file(filename=""):
+    """Funtion that read file and print as output
+
+    Parameters
+    ----------
+    filename : str
+        The file name to read and ptint
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         fileRead = f.read()
         print(fileRead, end="")
