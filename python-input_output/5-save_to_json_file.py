4bb71aaff6ce353729da7b04c4e1cbb06bccc13a
majdi aribi <majdi.developer@gmail.com>
2022-10-03 11:02:58 +0100

    :memo: function that writes an Object to a text file, using a JSON representation :memo:

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -18,7 +18,7 @@ def save_to_json_file(my_obj, filename):
         name of file to writes in.
 
     """
-    jsonStr = json.dumps(my_obj)
     with open(filename, 'w', encoding="utf-8") as f:
+        jsonStr = json.dumps(my_obj)
         f.write(jsonStr)
     f.close()
