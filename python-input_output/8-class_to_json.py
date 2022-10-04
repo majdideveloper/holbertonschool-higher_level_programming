0daad66b0a45da61d772d7fde2aa124022d927d3
majdi aribi <majdi.developer@gmail.com>
2022-10-04 13:51:33 +0100

    :memo:script that adds all arguments to a Python list, and then save them to a file :memo:

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-"""This module  contains  function that creates an Object from a “JSON file”"""
+"""This module  contains function that returns the dictionary description with
+simple data structure (list, dictionary, string, integer and boolean) for JSON
+serialization of an object"""
 
-import sys
 
 def class_to_json(obj):
+    """Function that creates an Object from a “JSON file”
+
+    Parameters
+    ----------
+        obj : object
+        any object
+    """
     return obj.__dict__
 
 
