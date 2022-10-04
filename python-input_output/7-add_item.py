465dec49e18175d00199e437c83a66d3475f5929
majdi aribi <majdi.developer@gmail.com>
2022-10-04 12:50:37 +0100

    :memo:script that adds all arguments to a Python list, and then save them to a file :memo:

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,9 +10,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 arg = sys.argv[1:]
 
-if len(sys.argv) == 1:
-    myList = []
-else:
+try:
     myList = load_from_json_file(filename)
+except FileNotFoundError:
+    myList = []
 myList.extend(arg)
 save_to_json_file(myList, filename)
