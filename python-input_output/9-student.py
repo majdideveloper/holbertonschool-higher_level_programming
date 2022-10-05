6e126a2b1a92cbe3fb9168a83d0a2c509f9212bc
majdi aribi <majdi.developer@gmail.com>
2022-10-05 08:43:10 +0100

    :memo:class Student that defines a student by attributes :memo:

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -2,13 +2,14 @@
 """ class Student
 """
 
+
 class Student:
     """ My class Student
     """
 
     def __init__(self, first_name, last_name, age):
         """Function to initialize the attributes of the class.
-        """ 
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
