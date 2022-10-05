43bf130e087c78003e9e6f5dd20f91e6778c8b0f
majdi aribi <majdi.developer@gmail.com>
2022-10-05 11:33:00 +0100

    try to test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,7 +7,7 @@ class TestBase(unittest.TestCase):
      def test_no_arg(self):
         b1 = Base()
         b2 = Base()
-        self.assertEqual(b1.id, 1)
+        self.assertEqual(b1.id, b2.id - 1)
    
 if __name__ == '__main__':
     unittest.main()
