342f8aefd83a066f945a1d0983fcdf19a7f46ee2
majdi aribi <majdi.developer@gmail.com>
2022-10-05 10:14:36 +0100

    create class base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" class Base
+"""
+
+
+class Base:
+    """class Base
+    """
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Function that creates an Object from a “JSON file”
+
+        Parameters
+        ----------
+        id : int
+        """
+        
+        if id is None:
+            Base.__nb_objects += 1 
+            self.id = self.__nb_objects
+        else:
+            self.id = id
