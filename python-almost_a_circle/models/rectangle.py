e57a51bd99a502966a28f45e22caf67041f022f9
majdi aribi <majdi.developer@gmail.com>
2022-10-05 14:44:44 +0100

    create class rectangle inherits Base class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -62,9 +62,9 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -74,7 +74,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
