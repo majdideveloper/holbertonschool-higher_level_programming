87826bcd3ff9613ea4c835832d7dcc0effbd7b15
majdi aribi <majdi.developer@gmail.com>
2022-10-06 08:11:54 +0100

    class square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -46,3 +46,26 @@ class Square(Rectangle):
         """print the str of Rectangle"""
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """Function that update Rectangle with value of args"""
+        if len(args) != 0:
+            for index in range(len(args)):
+                if index == 0:
+                    self.id = args[index]
+                elif index == 1:
+                    self.size = args[index]
+                elif index == 2:
+                    self.x = args[index]
+                elif index == 3:
+                    self.y = args[index]
+        else:
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                elif k == "size":
+                    self.width = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
