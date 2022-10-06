49e680d86445cc377ea955078351289144fa9737
majdi aribi <majdi.developer@gmail.com>
2022-10-06 08:52:13 +0100

    pdate the class Base by adding the static method def to_json_string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """ class Base
 """
 
+import json
+
 
 class Base:
     """class Base
@@ -21,3 +23,9 @@ class Base:
             self.id = self.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
