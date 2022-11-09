abd895ff4734d66bef126a63e2b2f617c5f1fa72
majdi aribi <majdi.developer@gmail.com>
2022-11-09 16:03:37 +0100

    script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATEE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT UNIQUE,
+CREATEE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT UNIQUE,
   name VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id);
+  PRIMARY KEY(id));
