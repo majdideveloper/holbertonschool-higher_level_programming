121a54296b116067b925b9a6a7e955483ad46821
majdi aribi <majdi.developer@gmail.com>
2022-12-05 21:01:15 +0100

    script that prints the addition of 2 integers

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-function add(a, b){
-  if (isNaN(1) || isNaN(b)){
+function add (a, b) {
+  if (isNaN(1) || isNaN(b)) {
     return NaN;
   } else {
-  return (Number(a) + Number(b));
+    return (Number(a) + Number(b));
   }
 }
-console.log(add(process.argv[2],process.argv[3]));
+console.log(add(process.argv[2], process.argv[3]));
