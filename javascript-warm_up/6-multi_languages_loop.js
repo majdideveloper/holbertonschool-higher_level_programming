8721b3a40b7b437507c8708b87f2e2ba567414a7
majdi aribi <majdi.developer@gmail.com>
2022-12-05 17:39:47 +0100

    script that prints 3 lines: but by using an array of string and a loop

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 const sentences = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < sentences.length; i++) {
   console.log(sentences[i]);
