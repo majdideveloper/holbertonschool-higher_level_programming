a6f5c0af33f46638d03b297b81c6d475f308b467
majdi aribi <majdi.developer@gmail.com>
2022-12-05 16:41:49 +0100

    a script that prints the first argument passed to it

---
+++ javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const process = require('process');
+const argNum = process.argv.length;
+if (argNum === 2) {
+  console.log('No argument');
+} else if (argNum === 3) {
+  console.log('Argument found');
+} else if (argNum > 3) {
+  console.log('Arguments found');
+}
