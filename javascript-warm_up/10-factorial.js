40eae93e8e60f85a4f9eedaba769dfddca185a2b
majdi aribi <majdi.developer@gmail.com>
2022-12-05 21:38:11 +0100

    cript that computes and prints a factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-function factorial(n){
+function factorial (n) {
   if (process.argv.length === 2) return 1;
-  let mem = []
-   if (n == 0 || n == 1)
-    return 1;
-  if (mem[n] > 0)
-    return mem[n];
-  return mem[n] = factorial(n-1) * n;
-    }
- 
-console.log(factorial(process.argv[2]))
+  const mem = [];
+  if (n === 0 || n === 1) { return 1; }
+  if (mem[n] > 0) { return mem[n]; }
+  mem[n] = factorial(n - 1) * n;
+  return mem[n];
+}
 
+console.log(factorial(process.argv[2]));
