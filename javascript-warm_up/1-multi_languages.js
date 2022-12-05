b447d8e12883ad3980f3a11228c009114d10529a
majdi aribi <majdi.developer@gmail.com>
2022-12-05 16:02:25 +0100

    script that prints 3 lines

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVarC = 'C is fun';
+const myVarPython = 'Python is cool';
+const myVarJavascript = 'JavaScript is amazing';
+console.log(myVarC);
+console.log(myVarPython);
+console.log(myVarJavascript);
