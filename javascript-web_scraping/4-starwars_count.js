ff428f1e5293c249556304eafe8b61604ed508f3
devmaj <majdi.developer@gmail.com>
2022-12-12 11:01:47 -0500

    Web scraping read file

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = `${process.argv[2]}`;
 const id = 'https://swapi-api.hbtn.io/api/people/18/';
 request(url, function (error, response, body) {
   if (error) console.error('error:', error);
