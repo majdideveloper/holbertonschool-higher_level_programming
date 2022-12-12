97ab515036af9605da048357f7bf9e8acb14743f
devmaj <majdi.developer@gmail.com>
2022-12-12 04:03:45 -0500

    class Rectangle that defines a rectangle

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,14 +1,4 @@
 #!/usr/bin/node
-class Rectangle {
-
-  name;
-
-  constructor(name) {
-    this.name = name;
-  }
-
-  introduceSelf() {
-    console.log(`Hi! I'm ${this.name}`);
-  }
-
-}
+module.exports = class Rectangle {
+  constructor() {}
+};
