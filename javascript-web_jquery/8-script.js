5afc54c9d3ae8d4c8a14ce269fbb3b7af60966df
devmaj <majdi.developer@gmail.com>
2022-12-13 05:06:31 -0500

    all Tasks

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,4 +1,12 @@
-
-$.get('https://swapi.co/api/films/?format=json', function (data) {
-    $('UL#list_movies').append(...data.results.map(movie => `<li>${movie.title}</li>`));
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(response => response.json())
+  .then(data => {
+   let items = data.results;
+    for (let item of items){
+        $('UL#list_movies').append(`<li>${item.title}</li>`);
+    }
+    
+  })
+  .catch(error => {
+    console.log('An error occurred:', error);
   });
