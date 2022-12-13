c5327dddd9a06e9fe19f21579455c05fb98212a3
devmaj <majdi.developer@gmail.com>
2022-12-13 11:15:04 -0500

    all Tasks

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -6,12 +6,21 @@ const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 request(url, function (error, response, body) {
   if (!error) {
     const listChara = JSON.parse(body).characters;
+    for (chara of listChara){
+        request(chara, function(error, response, body){
+            if(!error) console.log(JSON.parse(body).name);
+        })
+    } 
 
-    listChara.map((urlcara) => {
+    
+  }
+});
+
+/*
+listChara.map((urlcara) => {
       request(urlcara, function (error, response, body) {
         if (error) console.error('error:', error);
         else console.log(JSON.parse(body).name);
       });
     });
-  }
-});
+    */
