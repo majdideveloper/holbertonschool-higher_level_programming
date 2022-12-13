a23a7649806c120e213a14a8d251e692bf064ff7
devmaj <majdi.developer@gmail.com>
2022-12-13 09:22:58 -0500

    Web scraping read file

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const url = `${process.argv[2]}`;
-const id = 'https://swapi-api.hbtn.io/api/people/18/';
+
 request(url, function (error, response, body) {
   if (error) console.error('error:', error);
   const datas = JSON.parse(body).results;
@@ -10,8 +10,11 @@ request(url, function (error, response, body) {
   let num = 0;
   for (const data of datas) {
     data.characters.map((cara) => {
-      if (cara.endsWith("/18/")) num++;
-    })
+      if (cara.endsWith('/18/')) num++;
+      return num;
+    }
+    );
   }
-  console.log(num);  
+  console.log(num);
+  return num;
 });
